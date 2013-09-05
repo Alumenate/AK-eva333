c0d9221a541f4fd849f93a6b16cbee31861542ae
anarkia1976 <stefano.villa1976@gmail.com>
2013-09-05 17:59:33 +0200

    Revert "deadline-iosched: reduce -50% fifo_batch for a better latency"
    This reverts commit 2c93a84bbd341a2627af496af4bcc08cef483b74.

--- block/deadline-iosched.c
+++ block/deadline-iosched.c
@@ -20,7 +20,7 @@
 static const int read_expire = 32;  /* max time before a read is submitted. */
 static const int write_expire = 320; /* ditto for writes, these limits are SOFT! */
 static const int writes_starved = 1;    /* max times reads can starve a write */
-static const int fifo_batch = 4;       /* # of sequential requests treated as one
+static const int fifo_batch = 8;       /* # of sequential requests treated as one
 				     by the above parameters. For throughput. */
 
 struct deadline_data {
