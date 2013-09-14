b2147fbc55100c09d7d99df0a0299252acc943fe
anarkia1976 <stefano.villa1976@gmail.com>
2013-09-14 13:51:51 +0200

    ak-mako-build: compress ramdisk with gzip

--- ak-mako-build.sh
+++ ak-mako-build.sh
@@ -83,8 +83,8 @@ find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
 echo
 
 cd $INIT_DIR
-#find . | cpio -o -H newc | gzip -9 > ../initrd.img
-find . \( ! -regex '.*/\..*' \) | cpio -o -H newc -R root:root | xz --check=crc32 --lzma2=dict=8MiB > ../initrd.img
+#find . \( ! -regex '.*/\..*' \) | cpio -o -H newc -R root:root | xz --check=crc32 --lzma2=dict=8MiB > ../initrd.img
+find . \( ! -regex '.*/\..*' \) | cpio -o -H newc -R root:root | gzip -9 > ../initrd.img
 
 cd ../
 cp -vr $ZIMAGE_DIR/zImage .
