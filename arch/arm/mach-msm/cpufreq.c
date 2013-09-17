1e489b594fb77e0b5201aede8e49ef641ea5429a
anarkia1976 <stefano.villa1976@gmail.com>
2013-09-17 12:43:03 +0200

    cpufreq: add policy->max boot_freq

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -279,6 +279,7 @@ static int __cpuinit msm_cpufreq_init(struct cpufreq_policy *policy)
 	}
 
 	policy->cur = cur_freq;
+	policy->max = boot_freq;
 
 	policy->cpuinfo.transition_latency =
 		acpuclk_get_switch_time() * NSEC_PER_USEC;
